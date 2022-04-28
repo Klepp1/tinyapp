50b574099cd2c0c96ea157e08c4ffe613a60974c
Kyler <kylerlepp8@gmail.com>
2022-04-28 12:11:06 -0700

    added new route for urls/new and new ejs file for the page and also installed body parser and applied it to the app

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,6 +26,9 @@ app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render('urls_index', templateVars);
 })
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[`${req.params.shortURL}`] };
