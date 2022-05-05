9ada502e39916e24ad29a553a484a8583a874a46
Kyler <kylerlepp8@gmail.com>
2022-05-05 13:30:29 -0700

    added comments about each route

--- express_server.js
+++ express_server.js
@@ -35,17 +35,17 @@ const users = {
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-
+// this is the route for the login page
 app.get('/urls/login', (req, res) => {
   const templateVars = { user: users[req.session.user_id]};
   res.render('urls_login', templateVars);
 });
-
+//this is the route for the home page/ urls page
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlsForUser(req.session.user_id), user: users[req.session.user_id] };
   res.render('urls_index', templateVars);
 });
-
+//this route is for creating new shortURLS page
 app.get('/urls/new', (req, res) => {
   const templateVars = {user: users[req.session.user_id] };
   if (!users[req.session.user_id]) {
@@ -54,17 +54,17 @@ app.get('/urls/new', (req, res) => {
     res.render('urls_new', templateVars);
   }
 });
-
+// this route is for rendering the register page
 app.get('/urls/register', (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
   res.render('urls_register', templateVars);
 });
-
+// this route is for redirecting you to the website of the shortURL
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[`${req.params.shortURL}`]['longURL'];
   res.redirect(longURL);
 });
-
+// this route is for actually creating the new shortURL when you hit submit
 app.post('/urls', (req, res) => {
   if (!users[req.session.user_id]['id']) {
     res.status(403).send('You must login to create a TinyURL');
@@ -76,7 +76,7 @@ app.post('/urls', (req, res) => {
   };
   res.redirect(`/urls/${rString}`);
 });
-
+// this route is for taking you to the page of the shortURL where you can edit
 app.get("/urls/:shortURL", (req, res) => {
   if (!req.session.user_id) {
     res.status(403).send('This is not your URL!');
@@ -86,7 +86,7 @@ app.get("/urls/:shortURL", (req, res) => {
     res.render("urls_show", templateVars);
   }
 });
-
+// this route is the functionality of actually editing a shortURL to something different
 app.post('/urls/:shortURL', (req, res) => {
   if (urlDatabase[req.params.shortURL]['id'] === req.session.user_id['id']) {
     urlDatabase[req.params.shortURL]['longURL'] = req.body['New URL'];
@@ -95,7 +95,7 @@ app.post('/urls/:shortURL', (req, res) => {
     res.status(403).send('This is not your URL!');
   }
 });
-
+// this route is for when you click the delete button on a shortURL
 app.post('/urls/:shortURL/delete', (req, res) => {
   if (!req.session.user_id) {
     res.status(403).send('This is not your URL!');
@@ -105,7 +105,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
     res.redirect('http://localhost:8080/urls');
   }
 });
-
+// this route is for when someone logs into the server or tries too
 app.post('/urls/login/new', (req, res) => {
   let id = getUserByEmail(req.body.email, users);
   if (!getUserByEmail(req.body.email, users) || !bcrypt.compareSync(req.body.password, users[id]['password'])) {
@@ -117,12 +117,12 @@ app.post('/urls/login/new', (req, res) => {
     }
   }
 });
-
+// this simple route clears the session when you click logout
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect(302, '/urls');
 });
-
+// this route is for when a new user clicks register
 app.post('/urls/register/newUser', (req, res) => {
   const random = generateRandomString(11, '1234567890');
   if (!req.body.email || !req.body.password) {
